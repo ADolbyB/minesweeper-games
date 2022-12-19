75a75248a38766ee343b4d53d104d185f4ea87ba
Joel Brigida <joel@drtechreps.com>
2022-12-19 14:59:53 -0500

    Displays random  empty-block and bomb spaces

---
+++ board.py
@@ -0,0 +1,24 @@
+from piece import Piece
+from random import random
+
+class Board():
+    def __init__(self, size, prob):
+        self.size = size
+        self.prob = prob
+        self.setBoard()
+
+    def setBoard(self):
+        self.board = []
+        for row in range(self.size[0]):
+            row = []
+            for col in range(self.size[1]):
+                hasbomb = random() < self.prob
+                piece = Piece(hasbomb) # each piece on the minesweeper board.
+                row.append(piece)
+            self.board.append(row)
+
+    def getSize(self):
+        return self.size
+
+    def getPiece(self, index):
+        return self.board[index[0]][index[1]] # get row and column of the board array
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from game import Game
+from board import Board
 
-board = None
-screenSize = (1280, 720)
+size = (9, 9) # Board Size
+prob = 0.5 # bomb probability per section
+board = Board(size, prob)
+screenSize = (600, 600)
+
 game = Game(board, screenSize)
 game.run()
