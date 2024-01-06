e5d2b616b03d37a2284e6ceb21b0b9e3e0eea60e
Joel Brigida <joel@drtechreps.com>
2024-01-05 21:13:18 -0500

    Normalize Screen Size, Board Size, and Difficulty

--- pygame-minesweeper/main.py
+++ pygame-minesweeper/main.py
@@ -1,10 +1,10 @@
 from game import Game
 from board import Board
 
-size = (20, 50) # Board Size
-prob = 0.02 # bomb probability per section
+size = (10, 10) # Board Size
+prob = 0.2 # bomb probability per section
 board = Board(size, prob)
-screenSize = (1700, 800)
+screenSize = (700, 700)
 
 game = Game(board, screenSize)
 game.run()
